5cc03d4236ae3de2daa80594f270bbc7e99bb4db
Zachary Conlyn <tritone@gmail.com>
2016-12-14 18:49:53 -0500

    updated RefString class

--- RefString.java
+++ RefString.java
@@ -2,15 +2,26 @@ import java.util.ArrayList;
 
 class RefString {
 	ArrayList<Integer> refString;
-	
+	int next;	
+
 	RefString() {
 		refString = new ArrayList<Integer>();
+		next = 0;
 	}
-	
+
 	RefString(ArrayList<Integer> rs) {
 		refString = rs;
+		next = 0;
 	}
-	
+	int getLength() {
+		return refString.size();
+	}
+	int getNext() {
+		if (next < refString.size()) {
+			return refString.get(next++);
+		}
+		return -1;
+	}
 	void print() {
 		int i;
 		for (i = 0; i < refString.size() - 1; i++) {
